538d90aeed2d3c8dde5ac9aa29da3966bb5a3e1b
MariaReis19 <mariarreis19@etec.sp.gov.br>
2024-03-27 22:14:53 -0300

    estrutura de decisão

--- calculoIMC.php
+++ calculoIMC.php
@@ -1,18 +1,38 @@
 <?php 
 
 	$nome = "Maria";
-	$peso = 80;
-	$altura = 1.65;
+	$peso = 58;
+	$altura = 1.72;
 	$imc = $peso / ($altura * $altura);
 	
 	function calcular($peso, $altura){
 		return($peso / ($altura * $altura));
-		
-		
 
 	}
 
-	echo "Olá $nome, o seu peso é $peso Kg e sua altura é $altura metros e seu IMC é " . $imc;
+	printf("Olá $nome, o seu peso é $peso Kg e sua altura é $altura metros. <br> seu IMC é %.2f = " , $imc);
+
+			if($imc<=18.5){
+
+			echo("Abaixo do peso");
+
+		}elseif ($imc>18.5 && $imc<24.10) {
+
+			echo("Peso ideal");
+
+		}elseif ($imc>=25.0 && $imc<=29.9) {
+
+			echo("Levemente acima do peso");
+		}elseif ($imc>=30.0 && $imc<=34.9) {
+
+			echo("Obesidade grau I");
+
+		}elseif ($imc>=35.0 && $imc<=39.9) {
+
+			echo("Obesidade grau II(severa)");
+		}else {
+			echo("Obesidade III (Mórbida)");
+		}
 	
 
 
---
+++ exemplo11.php
@@ -0,0 +1,13 @@
+<?php 
+
+	$a = 15;
+	$b = 15;
+
+		if($b>$a){
+
+			echo("é verdade");
+		}else{
+
+			echo("é falso");
+		}
+ ?>
